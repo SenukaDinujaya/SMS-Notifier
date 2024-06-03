c36d7f2c02d0ef046495ebcf72bfe353c9a110f2
SenukaDinujaya <senuka.dinujaya@sgatechsolutions.com>
2024-06-03 13:51:28 -0500

    Changing the RUN thread

--- app/core/manager/thread_runner.py
+++ app/core/manager/thread_runner.py
@@ -4,48 +4,48 @@ from time import sleep
 import threading
 
 #SMS Sender Session
-# class Run:
-#     def __init__(self, item:Item) -> None:
-#         self.run_it = True
-
-#         self.sender = SMSSender(
-#             user_name=item.name,password=item.password,
-#             sender_did=item.did,call_duration=item.call_duration,
-#             message=item.message,timezone_diff=item.timezone_diff,log=True)
-#         self.item = item
-#         self.thread = threading.Thread(target=self.run, name=item.name)
-#         self.thread.start()
-
-#     def run(self):
-#         while self.run_it:
-#             try:    
-#                 self.sender.run()
-#             except:
-#                 self.restart()
-
-#     def stop(self):
-#         self.run_it = False
-
-#     def restart(self):
-#         self.stop()
-#         sleep(5)
-#         self.run_it = True
-#         print('Restarting...')
-#         self.run()
-
-
 class Run:
     def __init__(self, item:Item) -> None:
         self.run_it = True
+
+        self.sender = SMSSender(
+            user_name=item.name,password=item.password,
+            sender_did=item.did,call_duration=item.call_duration,
+            message=item.message,timezone_diff=item.timezone_diff,log=True)
+        self.item = item
         self.thread = threading.Thread(target=self.run, name=item.name)
         self.thread.start()
 
     def run(self):
-        ### The core of voip_client will go here
         while self.run_it:
-            print('Hi')
-            sleep(1)
-            print('Bye')
+            try:    
+                self.sender.run()
+            except:
+                self.restart()
 
     def stop(self):
         self.run_it = False
+
+    def restart(self):
+        self.stop()
+        sleep(5)
+        self.run_it = True
+        print('Restarting...')
+        self.run()
+
+
+# class Run:
+#     def __init__(self, item:Item) -> None:
+#         self.run_it = True
+#         self.thread = threading.Thread(target=self.run, name=item.name)
+#         self.thread.start()
+
+#     def run(self):
+#         ### The core of voip_client will go here
+#         while self.run_it:
+#             print('Hi')
+#             sleep(1)
+#             print('Bye')
+
+#     def stop(self):
+#         self.run_it = False
