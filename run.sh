ea922fff802475196deb747afc3f728c6d1bedd6
SenukaDinujaya <senuka.dinujaya@sgatechsolutions.com>
2024-06-06 10:43:28 -0500

    run.sh bug fix

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#Activate the Virtual environment
-source /venv/bin/activate
+# Activate the Virtual environment
+source venv/bin/activate
+#!/bin/bash
 
-#Run the app
-screen
-python3 run.py
+# Start a new screen session
+screen -dmS Notifier
+
+# Send the command to run your Python app in the screen session
+clear
+screen -S Notifier -X stuff "python3 run.py$(printf \\r)"
+
+# Attach to the screen session to interact with the Python app
+screen -r Notifier
