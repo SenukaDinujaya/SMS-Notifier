34ae906d66b8b75cb4694c9bad82e2c9e2b1f99d
SenukaDinujaya <senuka.dinujaya@sgatechsolutions.com>
2024-06-12 16:44:49 -0500

    Update or Manager

--- app/core/manager/manager.py
+++ app/core/manager/manager.py
@@ -12,7 +12,10 @@ class Manager:
     def __init__(self) -> None:
         self.senders: Dict[str, SMSSender] = {}
         self.running = False
-        self.log_sender=LogSender()
+        self.log_sender = LogSender()
+        self.queue_thread = None
+        self.watchdog_thread = Thread(target=self.__watchdog__, daemon=True)
+        self.watchdog_thread.start()
 
     def add_to_queue(self, item: Item):
         item = copy.deepcopy(item)
@@ -36,31 +39,46 @@ class Manager:
                 for key in list(self.senders.keys()):
                     self.senders[key].run()
                 
+                
                 elapsed_time = time.time() - start_time
                 if elapsed_time < 10:
                     time.sleep(10 - elapsed_time)
-        except Exception as e:
-            self.log_sender.send_log(['Thread', time.time(), e])
-        finally:
-            self.log_sender.send_log(['Thread', time.time(), 'Stoped'])
 
+        except Exception as e:
+            self.log_sender.send_log(['Thread', time.time(), str(e)])
+        finally:
+            self.log_sender.send_log(['Thread', time.time(), 'Stopped'])
+
+    def __watchdog__(self):
+        while True:
+            if self.running and (self.queue_thread is None or not self.queue_thread.is_alive()):
+                self.log_sender.send_log(['Watchdog', time.time(), 'Queue thread stopped, restarting...'])
+                self.running = False
+                self.start_queue()
+            time.sleep(5)
 
     def start_queue(self):
         if not self.running:
             self.running = True
-            self.queue_thread = Thread(target=self.__run_queue__)
+            self.queue_thread = Thread(target=self.__run_queue__,daemon=True)
             self.queue_thread.start()
-            
+            self.log_sender.send_log(['Queue', time.time(), 'Queue thread started'])
 
     def stop_queue(self):
         self.running = False
         if hasattr(self, 'queue_thread') and self.queue_thread.is_alive():
             self.queue_thread.join()
+        self.log_sender.send_log(['Queue', time.time(), 'Queue thread stopped'])
 
     def stop(self, item: Item):
         if item.name in self.senders:
             del self.senders[item.name]
+            self.log_sender.send_log(['System', time.time(), list(self.senders.keys())])
             if not self.senders:
                 self.stop_queue()
-        self.log_sender.send_log([item.name, time.time(), 'Stoped'])
-        
+        self.log_sender.send_log([item.name, time.time(), 'Stopped'])
+
+    def restart_queue_thread(self):  # Not in use
+        self.stop_queue()
+        time.sleep(1)
+        self.start_queue()
